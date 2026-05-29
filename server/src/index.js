27dd20f5337399562c774b56bd790ff66d27962f
Jon Suguiyama <jon.suguiyama@gmail.com>
2026-05-29 19:42:39 -0300

    fix: explicitly serve index.html on root route

--- server/src/index.js
+++ server/src/index.js
@@ -132,7 +132,11 @@ app.delete('/tasks/:id', async (req, res) => {
 });
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, '..', 'public'))); 
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
