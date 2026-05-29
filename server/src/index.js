ea1b21468ef00b98d20090303e5a5b9804363414
Jon Suguiyama <jon.suguiyama@gmail.com>
2026-05-29 19:19:21 -0300

    fix: update express wildcard route syntax for express 5 compatibility

--- server/src/index.js
+++ server/src/index.js
@@ -134,8 +134,8 @@ app.delete('/tasks/:id', async (req, res) => {
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('(.*)', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.listen(port, () => {
