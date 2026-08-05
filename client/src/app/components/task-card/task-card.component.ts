24e9fb525e1b152588a2f92837efce140f33f7c0
Jon Suguiyama <jon.suguiyama@gmail.com>
2026-08-05 20:33:34 -0300

    feat: add keyboard-accessible task reordering

--- client/src/app/components/task-card/task-card.component.ts
+++ client/src/app/components/task-card/task-card.component.ts
@@ -11,7 +11,12 @@ import { Task } from '../../services/tasks.service';
 })
 export class TaskCardComponent {
   @Input({ required: true }) task!: Task;
+  @Input() isFirst = false;
+  @Input() isLast = false;
+
   @Output() toggle = new EventEmitter<void>();
   @Output() edit = new EventEmitter<void>();
   @Output() delete = new EventEmitter<void>();
+  @Output() moveUp = new EventEmitter<void>();
+  @Output() moveDown = new EventEmitter<void>();
 }
